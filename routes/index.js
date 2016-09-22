8d4856aee381e76e29086c5fb5d7bbb2a9fc2313
Nitish Hooda <nitish@whiite.co>
2016-09-22 12:27:45 +0530

    date key

--- routes/index.js
+++ routes/index.js
@@ -157,7 +157,7 @@ router.get('/date', function(req, res, next){
   var date = new Date();
   var zerohour_date = date.setHours(5,30,0,0);
   var date1= new Date(zerohour_date);
-  res.status(200).json(date1);
+  res.status(200).json({zerohour_Date: date1});
   });
 
 router.post('/vitals', function(req, res, next){
